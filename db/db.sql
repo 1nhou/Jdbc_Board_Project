fd3bd7bc4e51b7ff4b9ee8f96c1bd398de98cc38
1nhou <inhouuuuu@gmail.com>
2023-04-05 10:31:36 +0900

    JDBC Insert 테스트 쿼리추가

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,17 @@
 DROP database if exists text_board;
 create database text_board;
 use text_board;
+
+CREATE table article(
+id int unsigned not null primary key auto_increment,
+regDate DATETIME NOT NULL,
+updateDate DATETIME NOT NULL,
+title CHAR(100) NOT NULL,
+`body` TEXT NOT NULL
+);
+
+INSERT into article
+SET regDate = NOW(),
+updateDate = NOW(),
+title = CONCAT("제목", RAND()),
+`body` = CONCAT("내용", RAND());
