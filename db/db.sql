6aa79936dfed06375d033c35f9c4f53c452733c6
1nhou <inhouuuuu@gmail.com>
2023-04-06 14:57:05 +0900

    회원가입 기능 구현

--- db/db.sql
+++ db/db.sql
@@ -1,7 +1,11 @@
+# DB 생성
 DROP database if exists text_board;
 create database text_board;
+
+# DB 선택
 use text_board;
 
+# 게시물 테이블 생성
 CREATE table article(
 id int unsigned not null primary key auto_increment,
 regDate DATETIME NOT NULL,
@@ -10,8 +14,20 @@ title CHAR(100) NOT NULL,
 `body` TEXT NOT NULL
 );
 
+# 게시물 테이블 테스트 데이터
 INSERT into article
 SET regDate = NOW(),
 updateDate = NOW(),
 title = CONCAT("제목", RAND()),
 `body` = CONCAT("내용", RAND());
+
+# 멤버 테이블 생성
+CREATE table `member`(
+id int(10) unsigned not null primary key auto_increment,
+regDate DATETIME NOT NULL,
+updateDate DATETIME NOT NULL,
+loginId CHAR(20) NOT NULL,
+loginPw CHAR(100) NOT NULL,
+`name` CHAR(200) NOT NULL
+);
+
--- src/com/prac/exam/textBoard/App.java
+++ src/com/prac/exam/textBoard/App.java
@@ -55,8 +55,85 @@ public class App {
     sc.close();
   }
   private int doAction(Connection conn, Scanner sc, String cmd) {
+      if (cmd.equals("join")) {
+        String loginId;
+        String loginPw;
+        String loginPwConfirm;
+        String name;
 
-    if (cmd.equals("add")) {
+
+
+      System.out.println("== 회원 가입 ==");
+
+      // 로그인 아이디 입력
+      while (true) {
+        System.out.printf("로그인 아이디 : ");
+        loginId = sc.nextLine().trim();
+
+        if(loginId.length() == 0){
+          System.out.println("로그인 아이디를 입력해주세요.");
+          continue;
+        }
+        break;
+      }
+
+      // 로그인 비밀번호 입력
+      while (true) {
+        System.out.printf("로그인 비밀번호 : ");
+        loginPw = sc.nextLine().trim();
+
+        if(loginPw.length() == 0){
+          System.out.println("로그인 비밀번호를 입력해주세요.");
+          continue;
+        }
+
+        // 비밀번호 검증
+        boolean loginPwConfirmIsSame = true;
+
+        while (true) {
+          System.out.printf("로그인 비밀번호 확인 : ");
+          loginPwConfirm = sc.nextLine().trim();
+
+          if(loginPw.length() == 0){
+            System.out.println("로그인 비밀번호를 한번 더 입력해주세요.");
+            continue;
+          }
+          if(loginPw.equals(loginPwConfirm) == false) {
+            System.out.println("비밀번호가 일치하지 않습니다. 다시 입력 해주세요.");
+            loginPwConfirmIsSame = false;
+            break;
+          }
+          break;
+        }
+        if(loginPwConfirmIsSame) {
+          break;
+        }
+      }
+
+        //  이름 입력
+        while (true) {
+          System.out.printf("이름 : ");
+          name = sc.nextLine().trim();
+
+          if(name.length() == 0){
+            System.out.println("이름을 입력해주세요.");
+            continue;
+          }
+          break;
+        }
+
+      SecSql sql = new SecSql();
+      sql.append("INSERT INTO `member`");
+      sql.append("SET regDate = NOW()");
+      sql.append(", updateDate = NOW()");
+      sql.append(", loginId = ?", loginId);
+      sql.append(", loginPw = ?", loginPw);
+      sql.append(", name = ?", name);
+
+      int id = DBUtil.insert(conn, sql);
+      System.out.printf("%d번 회원이 생성 되었습니다.\n", id);
+    }
+      else if (cmd.equals("add")) {
       System.out.println("== 게시물 생성 ==");
       System.out.printf("제목 : ");
       String title = sc.nextLine();
@@ -75,9 +152,6 @@ public class App {
 
     }
     else if (cmd.equals("list")) {
-      PreparedStatement pstat = null;
-      ResultSet rs = null;
-
       List<Article> articles = new ArrayList<>();
 
       SecSql sql = new SecSql();
